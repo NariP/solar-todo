18b188921d189ccc2018f721c83109ce9ba77a94
DESKTOP-IS1973C\Admin <tto9208@naver.com>
2021-08-19 21:34:44 +0900

    feat: todo 지우기 기능 구현 #8
    - removeTodo 함수 handleRemove 안에 넣기
    - removeTodo 함수 수정

--- src/components/todo/template/list/item/TodoItem.tsx
+++ src/components/todo/template/list/item/TodoItem.tsx
@@ -77,7 +77,9 @@ const TodoItem = ({ toggleTodo, removeTodo, todo }: TodoItemProps) => {
     toggleTodo(id);
   };
 
-  const handleRemove = () => {};
+  const handleRemove = () => {
+    removeTodo(id);
+  };
 
   return (
     <TodoItemBlock>
